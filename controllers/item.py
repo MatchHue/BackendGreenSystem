06fd96485fd2b9ad557b1ff457ab3fff6606acdf
MatchHue <matthewpaul3358@gmail.com>
2022-01-31 04:45:38 +0000

    item controller

---
+++ controllers/item.py
@@ -0,0 +1,24 @@
+from models.item import Item
+from flask import Flask, render_template,request,redirect,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+@app.route('/create_item',methods=['POST'])
+def create_item():
+    data=request.form
+    newItem=Item(name=data['name'],quantity=data['quantity'],price=data['price'])
+    db.session.add(newItem)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/',methods=['GET'])
+def list_items():
+    items=Item.query.all()
+    return render_template('product.html',items=items)
+
+@app.route('/sortbyprice',methods=['GET'])
+def sort_by_price():
+    items=Item.query.order_by('price').asc()
+    return render_template('product.html',items=items)
