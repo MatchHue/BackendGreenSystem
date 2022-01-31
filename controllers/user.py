df915cb1df9114cebe37a2a5812d305ec7562c7c
MatchHue <matthewpaul3358@gmail.com>
2022-01-31 05:09:46 +0000

    user_items

--- controllers/user.py
+++ controllers/user.py
@@ -1,4 +1,7 @@
 from models import (User,db)
+from flask import Flask, render_template,request,redirect,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 @app.route('/get_all_users_json',methods=['GET'])
 def get_all_users_json():
@@ -18,3 +21,9 @@ def create_user(username,password):
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
+
+
+@app.route('/user_items/<username>',methods=['GET'])
+def get_user_items(username):
+    user_items=User.query.filter_by(username=username).first()
+    return render_template('products',user_items=user_items.items)
